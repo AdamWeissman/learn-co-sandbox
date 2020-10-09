58666fc5aaf8d507157951f0e729219a0d5ea9ff
Adam Weissman <adam@comedytragedyepic.com>
2020-10-09 20:24:56 +0000

    Automatically backed up by Learn

---
+++ 01_insert_cats_into_cats_table.sql
@@ -0,0 +1,7 @@
+INSERT INTO cats (name, age, breed)
+VALUES ("Another Stupid Cat", 7, "Twisty Cat");
+
+INSERT INTO cats (name, age, breed)
+VALUES ("Foot Biter", 21, "Were-Cat");
+
+SELECT DISTINCT name FROM cats;
